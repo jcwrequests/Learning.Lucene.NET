cbebd279f27e8918a621ed6c1f98a61181d89cff
jcwrequests@comcast.net <jcwrequests@comcast.net>
2012-11-06 18:50:13 -0500

    Initial Commit

---
+++ Lucene.NET/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lucene.Net.Documents;
+using LuceneSearch.Data;
+using LuceneSearch.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GoLucene.AddUpdateLuceneIndex(SampleDataRepository.GetAll());
+
+            var r = GoLucene.GetAllIndexRecords();
+            System.Diagnostics.Debugger.Break();
+                    
+        }
+        
+    }
+}
