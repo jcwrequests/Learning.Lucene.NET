5bd70775dd81593e577b584ac8c014c9e035b17d
jcwrequests@comcast.net <jcwrequests@comcast.net>
2012-11-25 19:48:33 -0500

    Adding Lucene Store
    This will be an enhanced FileDocumentStore that will be used primarly
    for projections.

---
+++ Lucene.NET/Storage/ISerializationStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.NET.Storage
+{
+    public interface ISerializationStrategy
+    {
+        void Serialize<TEntity>(TEntity entity, Stream stream);
+        TEntity Deserialize<TEntity>(Stream stream);
+        string GetEntityLocationPath<TEntity>(object key);
+    }
+}
---
+++ Lucene.NET/Storage/LucenStrategy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lokad.Cqrs.AtomicStorage;
+
+namespace Lucene.NET.Storage
+{
+    public class LucenStrategy : IDocumentStrategy
+    {
+        public TEntity Deserialize<TEntity>(System.IO.Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetEntityBucket<TEntity>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetEntityLocation<TEntity>(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Serialize<TEntity>(TEntity entity, System.IO.Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
